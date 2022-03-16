ea6936dc77b7075fefe590c7b7a2277ca4c1f340
ivgi <ivgiking333@gmail.com>
2022-03-16 19:12:25 +0200

    first commit

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
 import pygame
 from settings import *
-from tiles import Tile
+from level import Level
 pygame.init()
 screen = pygame.display.set_mode((screen_width, screen_height))
 clock = pygame.time.Clock()
+level = Level(level_map, screen)
 pygame.display.set_caption('celeste clone')
 runs = True
+
 while runs:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             runs = False
 
     screen.fill('black')
+    level.run()
+
     pygame.display.flip()
     clock.tick(60)
